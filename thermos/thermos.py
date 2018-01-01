3c3a3abedfbf6d4b6321586b88e8902d08fef10f
SantaAna <pstruthe@gmail.com>
2018-01-01 12:20:23 -0800

    Jinja changes added

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname  = lastname
+    
+    def __str__(self):
+        return "{} {}".format(self.firstname,self.lastname)
+    
+    def initials(self):
+        return "{}.{}.".format(self.firstname[0],self.lastname[0])
+ 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="This is my title", text="This is my text.", user=User("Pixie", "Goodog"))
 
 if __name__== '__main__':
     app.run(host='0.0.0.0')
